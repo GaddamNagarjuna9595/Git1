0a3e733f77def039a83c10e92304f64a1b49972b
HP <HP@DESKTOP-UTC920B>
2023-04-14 21:38:45 +0530

    fIRST Commiting..

---
+++ src/com/package1/Test3.java
@@ -0,0 +1,16 @@
+package com.package1;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+
+		String Name="RamCharan";
+		
+		if (Name.equals("RamCharan")) {
+			System.out.println("He iS the Wworld's Greatest LOVER");
+		}else {
+			System.out.println("Oscar Award WInner");
+		}
+	}
+
+}
